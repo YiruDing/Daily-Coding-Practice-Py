7d1ed7842ffd1533e8db7f4bd8b17eda05ee32e4
Yi-Ru Ding <dingyr0925@gmail.com>
2021-09-21 12:58:30 -0400

    0921

---
+++ 63.py
@@ -0,0 +1,56 @@
+# This problem was asked by Microsoft.
+
+# Given a 2D matrix of characters and a target word, write a function that returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+
+# For example, given the following matrix:
+
+# [['F', 'A', 'C', 'I'],
+#  ['O', 'B', 'Q', 'P'],
+#  ['A', 'N', 'O', 'B'],
+#  ['M', 'A', 'S', 'S']]
+# and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.
+
+class GFG:
+    def __init__(self):
+        self.R = None
+        self.C = None
+        self.dir = [[-1,0],[1,0],[1,1],[1,-1],[-1,-1],[-1,1],[0,1],[0,-1]]
+    
+    def search2D(self,grid,row,col,word):
+        if grid[row][col] != word[0]:
+            return False
+        for x,y in self.dir:
+            rd,cd = row + x,col + y
+            flag = True
+
+            for k in range(1,len(word)):
+                if (0 <= rd < self.R and 
+                    0 <= cd < self.C and
+                    word[k] == grid[rd][cd]):
+                    rd += x
+                    cd += y
+                else:
+                    flag = False
+                    break
+            if flag:
+                return True
+        return False
+    
+    def patternSearch(self,grid,word):
+        self.R = len(grid)
+        self.C = len(grid[0])
+
+        for row in range(self.R):
+            for col in range(self.C):
+                if self.search2D(grid,row,col,word):
+                    print("Pattern found at "+
+                    str(row) + ', ' + str(col))
+
+if __name__ == '__main__':
+    grid = ["GREEKSFORGREEKS",
+            "GREEKSQUIZGREEK",
+            "IDEQAPRACTICE"]
+        gfg =GFG()
+        gfg.patternSearch(grid,'GEEKS')
+        print('')
+        gfg.patternSearch(grid,'EEE') 
